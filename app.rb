8c2e7e975bdeb2f3abbfc8f898bc9824d9ff1d94
5ejrChr1st3ns3n <118183544+5ejrChr1st3ns3n@users.noreply.github.com>
2024-03-11 14:50:29 +0100

    eeeee

--- app.rb
+++ app.rb
@@ -13,15 +13,16 @@ end
 
 # Route to handle the file upload submission
 post '/upload' do
-  #unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-    #return "No file selected"
-  #end
+  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+    return "No file selected"
+  end
 
-  #target = "./uploads/#{name}"
-  #File.open(target, 'wb') { |f| f.write(tmpfile.read) }
+  target = "./uploads/#{name}"
+  File.open(target, 'wb') { |f| f.write(tmpfile.read) }
+  #File.write(target, tmpfile.read)
 
-  # Execute Ruby code
-  #HexFileProcessor::Program.main(target) # Pass the file path to the main method
+   #Execute Ruby code
+  HexFileProcessor::Program.fetch_hex_number_information(target) # Pass the file path to the main method
 
   redirect '/result'
 end
--- datafetcher.rb
+++ datafetcher.rb
@@ -1,28 +1,21 @@
  require 'net/http'
+ require 'sqlite3'
  require 'json'
 
  module HexFileProcessor
    class Program
-     def self.main(file_path)
-       if file_path.nil? || !File.exist?(file_path)
-         puts "Invalid file path."
-         return
-       end
-
-       fetch_hex_number_information(file_path)
-     end
-
      def self.fetch_hex_number_information(file_path)
        api_url = "http://api.monni.moe/map?k="
+
+       db = SQLite3::Database.new 'db/maps.db'
 
        puts "Processing file: #{file_path}"
 
        begin
          hex_numbers = File.readlines(file_path)
-         fetched_info_file_path = File.join(File.dirname(__FILE__), "fetched_info.txt")
 
-         hex_numbers.each do |hex_number|
-           url = api_url + hex_number.chomp
+         hex_numbers.each do |key|
+           url = api_url + key.chomp
 
            response = Net::HTTP.get_response(URI(url))
 
@@ -30,21 +23,13 @@
              response_body = response.body
              response_data = JSON.parse(response_body)
 
-             if response_data && response_data['versions']
-               max_version = response_data['versions'].max_by { |v| v['createdAt'] }
+             max_version = response_data['versions'].max_by { |v| v['createdAt'] }
+             created_at = max_version['createdAt']
+             metadata = response_data['metadata']
+             level_author_name = metadata['levelAuthorName']
 
-               if max_version
-                fetched_info = []
-            
-                created_at = max_version['createdAt']
-                metadata = response_data['metadata']
-            
-                level_author_name = metadata['levelAuthorName'] if metadata && metadata['levelAuthorName']
-            
-                # Assuming hex_number contains the hex code you want to insert
-                db.execute("INSERT INTO your_table (key, age, mapper) VALUES (hex_number, created_at, level_author_name)")
-               end
-             end
+             db.execute("DELETE * FROM maps")
+             db.execute("INSERT INTO maps (key, age, mapper) VALUES (?, ?, ?)", key.chomp, created_at, level_author_name)
            end
          end
        end
@@ -52,4 +37,4 @@
    end
  end
 
- HexFileProcessor::Program.main("./uploads/hex_numbers.txt")
+ HexFileProcessor::Program.fetch_hex_number_information("./uploads/hex_numbers.txt")
