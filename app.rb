58bd7507c73c54c5e143974ec39df32e3e8286c1
5ejrChr1st3ns3n <118183544+5ejrChr1st3ns3n@users.noreply.github.com>
2024-03-18 14:42:56 +0100

    aaaaaaaaa

--- app.rb
+++ app.rb
@@ -3,34 +3,10 @@ require 'sqlite3'
 require 'slim'
 require 'bcrypt'
 
-# Connect to SQLite database
 DB = SQLite3::Database.new 'db/maps.db'
 
-# Route to handle the file upload form
 get '/' do
-  slim :index
-end
 
-# Route to handle the file upload submission
-post '/upload' do
-  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-    return "No file selected"
-  end
-
-  target = "./uploads/#{name}"
-  File.open(target, 'wb') { |f| f.write(tmpfile.read) }
-  #File.write(target, tmpfile.read)
-
-  #Execute Ruby code
-  require_relative 'datafetcher.rb'
-  
-  redirect '/result'
-end
-
-# Route to display the processed data
-get '/result' do
-  @processed_data = DB.execute("SELECT * FROM maps")
-  slim :result
 end
 
 get("/register") do
@@ -41,14 +17,55 @@ post("/users/new") do
   username = params[:username]
   password = params[:password]
   password_confirm = params[:password_confirm]
-
+  
   if (password == password_confirm)
     password_digest = BCrypt::Password.create(password)
     db = SQLite3::Database.new("db/users.db")
     db.execute("INSERT INTO users (username, pwdigest) VALUES (?, ?)",username, password_digest)
     redirect("/")
-
+    
   else
     "Lösenorden matchar inte"
+  end
+end
+
+post("/login") do
+  username = params[:username]
+  password = params[:password]
+  
+  db = SQLite3::Database.new("db/users.db")
+  db.results_as_hash = true
+  
+  result = db.execute("SELECT * FROM users WHERE userame = ?, username").first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+  
+  if BCrypt::Password.new(pwdigest) == password
+    redirect("/home")
+    
+  else
+    "Incorrect Password"
+  end
+
+  # Route to handle the file upload form
+  get '/home' do
+    @processed_data = DB.execute("SELECT * FROM maps")
+
+    slim :index
+  end
+  
+  # Route to handle the file upload submission
+  post '/upload' do
+    unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+      return "No file selected"
+    end
+  
+    target = "./uploads/#{name}"
+    File.open(target, 'wb') { |f| f.write(tmpfile.read) }
+    #File.write(target, tmpfile.read)
+  
+    require_relative 'datafetcher.rb'
+    
+    redirect '/home'
   end
 end
